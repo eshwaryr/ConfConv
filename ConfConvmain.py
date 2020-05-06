3facea2c16856d9134ce1ed8c749aad5d4ef2a6c
eshwaryr <eshwaryr@gmail.com>
2020-05-06 12:11:08 +0530

    Update ConfConvmain.py

--- ConfConvmain.py
+++ ConfConvmain.py
@@ -2,7 +2,6 @@ import config_parse
 import config_convert
 
 # Parse input configuration files
-
 config_parse
 
 # Wait for user input whether to start the conversion code
@@ -10,5 +9,4 @@ print("Press Enter to start configuration conversion")
 input()
 
 # Create configuration file for new device (only if, yaml output from config_parse and jinja template are both ready)
-
 config_convert
